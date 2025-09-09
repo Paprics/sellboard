378b63d5e53bd3bd873369f5a7f499ebad3d8cfc
Valeriy D. <paprics@gmail.com>
2025-09-09 15:13:49 +0300

    Initial commit

---
+++ src/config/urls.py
@@ -0,0 +1,18 @@
+# urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from core import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+]
+
+# DEBUG TOOLBAR
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
